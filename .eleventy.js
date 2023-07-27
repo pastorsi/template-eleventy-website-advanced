ee1f230cc83554c16c35ce9b8fc3ce044624fcc1
Simon <pastorsi@gmail.com>
2023-07-27 21:20:20 +0100

    Added Bulma and a stylesheet for it with Pass Through Copy in eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,12 @@
 module.exports = function(eleventyConfig){
   eleventyConfig.addPassthroughCopy("src/assets/images");
+  
+  // Copy the `css` directory to the output
+  eleventyConfig.addPassthroughCopy('css');
+
+  // Watch the `css` directory for changes
+  eleventyConfig.addWatchTarget('css');
+
     return {
       dir: {
         input: "src",
--- src/index.md
+++ src/index.md
@@ -1,8 +1,9 @@
 ---
 layout: base
-title: Home
+title: "Home"
 ---
 ![hero image](assets/images/logo.png)
+
 # My MCEA Eleventy Site
 
 MCEA site   made with [Eleventy](https://www.11ty.io/)
