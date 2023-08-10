98ca1784e9c85ca1ab0b989b7731b432acd3cd3b
Simon <pastorsi@gmail.com>
2023-08-10 22:28:13 +0100

    Added Favicon with a favicon generation script implemented using sharp.

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const sharp = require('sharp')
+
 module.exports = function(eleventyConfig){
   eleventyConfig.addPassthroughCopy('src/assets/images');
   eleventyConfig.addPassthroughCopy('src/assets/js');
@@ -7,7 +9,23 @@ module.exports = function(eleventyConfig){
 	});
   // Obtain the `year` from the template
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
-  
+
+  // Generate favicon from svg input
+  // Only run on production build
+  // if (process.env.NODE_ENV === 'build') {
+    eleventyConfig.on('eleventy.before', async () => {
+      console.log('[11ty] Generating Favicon')
+      await sharp('src/assets/images/favicon.svg')
+        .png()
+        .resize(96, 96)
+        .toFile('_site/assets/images/icon-96x96.png')
+        .catch(function (err) {
+          console.log('[11ty] ERROR Generating favicon')
+          console.log(err)
+        })
+    })
+  // }
+  eleventyConfig.watchIgnores.add('_site/assets/images/icon-96x96.png')
 
     return {
       dir: {
@@ -17,5 +35,8 @@ module.exports = function(eleventyConfig){
         layouts: "_layouts"
       }
     };
+  
+    
   }
+  
   
