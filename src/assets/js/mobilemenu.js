9240746e2f18800054d05fb3b67afc49b070436c
Simon <pastorsi@gmail.com>
2024-09-25 21:18:44 +0100

    Nested menu
    Menu flexible enough for small and large websites

--- src/assets/js/mobilemenu.js
+++ src/assets/js/mobilemenu.js
@@ -1,7 +1,23 @@
-// mobile menu
+// mobile menu toggle
 const burgerIcon = document.querySelector('#burger');
 const navbarMenu = document.querySelector('#nav-links');
 
+// Toggle the mobile menu (burger menu)
 burgerIcon.addEventListener('click', () => {
   navbarMenu.classList.toggle('is-active');
+});
+
+// Toggle submenus on mobile
+const dropdowns = document.querySelectorAll('.navbar-item.has-dropdown');
+
+dropdowns.forEach(dropdown => {
+  const dropdownLink = dropdown.querySelector('.navbar-link');
+  
+  dropdownLink.addEventListener('click', (e) => {
+    e.preventDefault(); // Prevent default anchor behavior
+    const isActive = dropdown.classList.toggle('is-active');
+    
+    // Update the aria-expanded attribute based on the dropdown's active state
+    dropdownLink.setAttribute('aria-expanded', isActive ? 'true' : 'false');
+  });
 });
